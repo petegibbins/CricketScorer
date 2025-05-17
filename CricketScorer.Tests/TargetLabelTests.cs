e3a0854f7d492c86413a6aea110f44347af7d3ae
peteg <peteg@REDDOG1>
2025-05-17 01:08:55 +0100

    Add tests for TargetLabelFormatter functionality

---
+++ CricketScorer.Tests/TargetLabelTests.cs
@@ -0,0 +1,73 @@
+﻿using CricketScorer.Core.Models;
+using CricketScorer.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketScorer.Tests
+{
+    [TestClass]
+    public sealed class TargetLabelFormatterTests
+    {
+        private readonly Formatter _summaryService = new();
+
+        [TestMethod]
+        public void TargetLabel_Shows_NeedsMoreToWin()
+        {
+            var match = new Match
+            {
+                TeamAScore = 80,
+                Runs = 75,
+                TeamB = "Opposition"
+            };
+
+            var result = _summaryService.FormatTargetLabel(match);
+            Assert.AreEqual("Needs 6 more to win", result);
+        }
+
+        [TestMethod]
+        public void TargetLabel_Shows_ScoresLevel()
+        {
+            var match = new Match
+            {
+                TeamAScore = 80,
+                Runs = 80,
+                TeamB = "Opposition"
+            };
+
+            var result = _summaryService.FormatTargetLabel(match);
+            Assert.AreEqual("Scores level!", result);
+        }
+
+        [TestMethod]
+        public void TargetLabel_Shows_CurrentlyAhead_Singular()
+        {
+            var match = new Match
+            {
+                TeamAScore = 80,
+                Runs = 81,
+                TeamB = "Opposition"
+            };
+
+            var result = _summaryService.FormatTargetLabel(match);
+            Assert.AreEqual("Opposition is currently ahead by 1 run.", result);
+        }
+
+        [TestMethod]
+        public void TargetLabel_Shows_CurrentlyAhead_Plural()
+        {
+            var match = new Match
+            {
+                TeamAScore = 80,
+                Runs = 85,
+                TeamB = "Opposition"
+            };
+
+            var result = _summaryService.FormatTargetLabel(match);
+            Assert.AreEqual("Opposition is currently ahead by 5 runs.", result);
+        }
+    }
+
+}
